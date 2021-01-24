65c90ad08826460efc6967bfebc843bf35927185
s163404 <s163404@gmail.com>
2021-01-24 22:55:45 +0900

    キーボード高さ分画面を上げる

--- SampleUserDefaults/TextFieldViewController.swift
+++ SampleUserDefaults/TextFieldViewController.swift
@@ -10,22 +10,32 @@ import UIKit
 
 class TextFieldViewController: UIViewController {
 
-    let textField = UITextField()
+    let textField1 = UITextField()
+    let textField2 = UITextField()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         
-        view.addSubview(textField)
+        view.addSubview(textField1)
+        view.addSubview(textField2)
 
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        textField.placeholder = "テキスト"
-        textField.borderStyle = .roundedRect
+        textField1.translatesAutoresizingMaskIntoConstraints = false
+        textField1.placeholder = "テキスト"
+        textField1.borderStyle = .roundedRect
+        textField1.delegate = self
+        
+        textField2.translatesAutoresizingMaskIntoConstraints = false
+        textField2.placeholder = "粕谷秀樹"
+        textField2.borderStyle = .roundedRect
+        textField2.delegate = self
         
         NSLayoutConstraint.activate([
-            textField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            textField.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -view.bounds.height * 1/5),
+            textField1.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            textField1.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -view.bounds.height * 1/5),
+            textField2.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            textField2.topAnchor.constraint(equalTo: textField1.bottomAnchor, constant: -view.bounds.height * 1/5),
         ])
         
         
@@ -44,12 +54,23 @@ class TextFieldViewController: UIViewController {
 
     // キーボードが表示された時
     @objc private func keyboardWillShow(sender: NSNotification) {
-        if textField.isFirstResponder {
+        
+        let firstResponder = textField1.isFirstResponder ? textField1 : textField2
+        
+        
+        if textField1.isFirstResponder || textField2.isFirstResponder {
             guard let userInfo = sender.userInfo else { return }
+            
+            let keyboardFrameInfo = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+            let keyboardSize = keyboardFrameInfo.cgRectValue.size
             let duration: Float = (userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! NSNumber).floatValue
+
             UIView.animate(withDuration: TimeInterval(duration), animations: { () -> Void in
-                let transform = CGAffineTransform(translationX: 0, y: -200)
-                self.view.transform = transform
+//                let transform = CGAffineTransform(translationX: 0, y: -keyboardSize.height)
+//                self.view.transform = transform
+                if self.view.frame.origin.y == 0 {
+                    self.view.frame.origin.y -= keyboardSize.height
+                }
             })
         }
     }
@@ -59,8 +80,18 @@ class TextFieldViewController: UIViewController {
         guard let userInfo = sender.userInfo else { return }
         let duration: Float = (userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! NSNumber).floatValue
         UIView.animate(withDuration: TimeInterval(duration), animations: { () -> Void in
-            self.view.transform = CGAffineTransform.identity
+//            self.view.transform = CGAffineTransform.identity
+            if self.view.frame.origin.y != 0 {
+                self.view.frame.origin.y = 0
+            }
         })
     }
 
+}
+
+extension TextFieldViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
